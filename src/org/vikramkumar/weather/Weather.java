8e7492d1340340c4a602dd73df3ec34735a0a542
coolviki <vikram17june@gmail.com>
2017-04-30 23:02:33 +0530

    Adding last updated time to the API

--- src/org/vikramkumar/weather/Weather.java
+++ src/org/vikramkumar/weather/Weather.java
@@ -20,10 +20,11 @@ public class Weather {
 
 			 String overAllStatusTitle = doc.getElementById("aqiwgtvalue").attr("title").toString();
 			 String overAllAQIValue = doc.getElementById("aqiwgtvalue").text();
+			 String updatedAtTime = doc.getElementById("aqiwgtutime").text();
 
 			 String pmi25 = doc.getElementById("cur_pm25").text();
 			 String pmi10 = doc.getElementById("cur_pm10").text();
-			 returnHtml.append("<html>").append("Mandir Marg: Delhi - ").append("OverAll Status: <b>").append(overAllStatusTitle).append("</b> and overall AQI: <b>").append(overAllAQIValue).append("</b> <br>PMI-2.5: ").append(pmi25).append(",<br> PMI-10: ").append(pmi10).append("<br><br> PMI 2.5, <br>0-50 = Good,<br> 51-100 = Moderate,<br>101-150 = Moderately Unhealhty, <br> 150 - 200 = Unhealthy, <br> 201-300: Very Unhealthy<br> 300+: Hazadaorus").append("</html>");
+			 returnHtml.append("<html>").append("Mandir Marg: Delhi - ").append("OverAll Status: <b>").append(overAllStatusTitle).append("</b> and overall AQI: <b>").append(overAllAQIValue).append("</b> <br>PMI-2.5: ").append(pmi25).append("as of ").append(updatedAtTime).append("<br> PMI-10: ").append(pmi10).append("<br><br> PMI 2.5, <br>0-50 = Good,<br> 51-100 = Moderate,<br>101-150 = Moderately Unhealhty, <br> 150 - 200 = Unhealthy, <br> 201-300: Very Unhealthy<br> 300+: Hazadaorus").append("</html>");
 			 returnValue=returnHtml.toString();
 			 
 			} catch (Exception ee){
@@ -42,10 +43,11 @@ public class Weather {
 
 		 String overAllStatusTitle = doc.getElementById("aqiwgtvalue").attr("title").toString();
 		 String overAllAQIValue = doc.getElementById("aqiwgtvalue").text();
+		 String updatedAtTime = doc.getElementById("aqiwgtutime").text();
 
 		 String pmi25 = doc.getElementById("cur_pm25").text();
 		 String pmi10 = doc.getElementById("cur_pm10").text();
-		 returnHtml.append("").append("Mandir Marg: Delhi - ").append("OverAll Status: ").append(overAllStatusTitle).append(" and overall AQI: ").append(overAllAQIValue).append(" Visit http://pollution.vikramkumar.org for details.");
+		 returnHtml.append("").append("Mandir Marg: Delhi - ").append("OverAll Status: ").append(overAllStatusTitle).append(" and overall AQI: ").append(overAllAQIValue).append(" as of ").append(updatedAtTime).append(" Visit http://pollution.vikramkumar.org for details.");
 		 returnValue=returnHtml.toString();
 		 
 		} catch (Exception ee){
